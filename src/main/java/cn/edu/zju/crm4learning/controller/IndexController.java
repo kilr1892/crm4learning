15cc4753c9b763e10a74f97376d4faac2103fcd9
richard_glb <kilr1892@163.com>
2019-08-01 17:00:58 +0800

    终于把SSM框架搭起来了...

---
+++ src/main/java/cn/edu/zju/crm4learning/controller/IndexController.java
@@ -0,0 +1,23 @@
+package cn.edu.zju.crm4learning.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 首页controller.
+ * <p>
+ * When I wrote this, only God and I understood what I was doing
+ * Now, God only knows
+ *
+ * @author RichardLee
+ * @version v1.0
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    private String showIndex(Model model) {
+        return "index";
+    }
+}
